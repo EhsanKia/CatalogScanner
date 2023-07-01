c0756a70d0f0e0e59eea75bf702a63782f911dad
lin <carolin.svensson@live.se>
2023-07-01 18:14:02 +0200

    Removed integration, connected bot to nook.lol

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import os
 import platform
 import contextlib
+import hashids
+import datetime
+from google.cloud import datastore
 
 import discord
 from discord.ext import commands
@@ -9,18 +12,48 @@ from dotenv import load_dotenv
 
 import scanner
 load_dotenv()
-DISCORD_TOKEN  = os.getenv('DISCORD_TOKEN')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+SALT = os.getenv('SALT')
 
 intents = discord.Intents.default()
 bot = commands.Bot(intents=intents)
 
-async def handle_message(ctx: discord.ApplicationContext, attachment: discord.Attachment, integration: str):
+hashid_client = hashids.Hashids(salt=SALT, min_length=6)
+
+def upload_to_datastore(result, discord_user_id=None):
+    print(f'Uploading to datastore...')
+
+    datastore_client = datastore.Client.from_service_account_json(
+        'catalog-scanner.json')
+
+    temp_key = datastore_client.key('Catalog')
+    key = datastore_client.allocate_ids(temp_key, 1)[0]
+    catalog = datastore.Entity(key, exclude_from_indexes=['data', 'unmatched'])
+
+    key_hash = hashid_client.encode(key.id)
+    catalog_data = '\n'.join(result.items).encode('utf-8')
+    catalog.update({
+        'hash': key_hash,
+        'data': catalog_data,
+        'locale': result.locale,
+        'type': result.mode.name.lower(),
+        'created': datetime.datetime.utcnow(),
+        'discord_user_id': discord_user_id
+    })
+    if result.unmatched:
+        unmatched_data = '\n'.join(result.unmatched).encode('utf-8')
+        catalog['unmatched'] = unmatched_data
+    
+    datastore_client.put(catalog)
+    return catalog
+
+async def handle_message(ctx: discord.ApplicationContext, attachment: discord.Attachment):
 	attch_type = attachment.content_type.split('/') # Attachment.content_type returns a {type}/{file_format} string
 	if attch_type[0] != 'video':
 		await ctx.respond('The attachment needs to be a valid video file', ephemeral=True)
 	else:
 		try:
-			await ctx.respond('Scan started, your results will be ready soon', ephemeral=True)
+			await ctx.respond('Scan started, your results will be ready soon...', ephemeral=True)
 			file = await attachment.to_file()
 			path = 'dump'
 			if not os.path.exists(path):
@@ -32,8 +65,9 @@ async def handle_message(ctx: discord.ApplicationContext, attachment: discord.At
 			with contextlib.suppress(FileNotFoundError):
 				os.remove(temp_file)
 
-			# Do cool redirect stuff here!
-			await ctx.respond(f"Found {len(result.items)} items in your video.\nResults:", ephemeral=True)
+			catalog = upload_to_datastore(result, ctx.user.id)
+			url = 'https://nook.lol/{}'.format(catalog['hash'])
+			await ctx.interaction.edit_original_response(content=f"Found {len(result.items)} items in your video.\nResults: {url}")
 		except Exception as e:
 			print(f'Failed to scan video {e}')
 			await ctx.respond('Failed to scan video. Make sure you have a valid video file.', ephemeral=True)
@@ -48,12 +82,10 @@ async def on_ready():
     
 @bot.slash_command(name="scan", description="Extracts your Animal Crossing catalog items and DIY recipes from a recorded video.")
 @option("attachment",discord.Attachment,description="The video to scan", required=True)
-@option("integration", description="Visit https://nook.lol/ to see available hashtags", required=False)
 async def scan(ctx: discord.ApplicationContext, attachment: discord.Attachment, integration: str):
 	if attachment:
 		await handle_message(ctx, attachment, integration)
 	else:
 		await ctx.respond('No attachment found.', ephemeral=True)
 
-# EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
 bot.run(DISCORD_TOKEN)
