e4efdb61d06d833e4feed9de1f44c48f3340c2cc
lin <carolin.svensson@live.se>
2023-07-03 09:04:39 +0200

    Removed .env and added constants.py, formatting

--- bot.py
+++ bot.py
@@ -8,17 +8,15 @@ from google.cloud import datastore
 import discord
 from discord.ext import commands
 from discord import option
-from dotenv import load_dotenv
 
+import constants
 import scanner
-load_dotenv()
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-SALT = os.getenv('SALT')
 
 intents = discord.Intents.default()
 bot = commands.Bot(intents=intents)
 
-hashid_client = hashids.Hashids(salt=SALT, min_length=6)
+hashid_client = hashids.Hashids(salt=constants.SALT, min_length=6)
+
 
 def upload_to_datastore(result, discord_user_id=None):
     print(f'Uploading to datastore...')
@@ -43,34 +41,39 @@ def upload_to_datastore(result, discord_user_id=None):
     if result.unmatched:
         unmatched_data = '\n'.join(result.unmatched).encode('utf-8')
         catalog['unmatched'] = unmatched_data
-    
+
     datastore_client.put(catalog)
     return catalog
 
+
 async def handle_message(ctx: discord.ApplicationContext, attachment: discord.Attachment):
-	attch_type = attachment.content_type.split('/') # Attachment.content_type returns a {type}/{file_format} string
-	if attch_type[0] != 'video':
-		await ctx.respond('The attachment needs to be a valid video file', ephemeral=True)
-	else:
-		try:
-			await ctx.respond('Scan started, your results will be ready soon...', ephemeral=True)
-			file = await attachment.to_file()
-			path = 'dump'
-			if not os.path.exists(path):
-				os.makedirs(path)
-			temp_file = os.path.join(path, file.filename)
-			await attachment.save(temp_file)
+    # Attachment.content_type returns a {type}/{file_format} string
+    attch_type = attachment.content_type.split('/')
+    if attch_type[0] != 'video':
+        await ctx.respond('The attachment needs to be a valid video file', ephemeral=True)
+        return
 
-			result = scanner.scan_media(temp_file)
-			with contextlib.suppress(FileNotFoundError):
-				os.remove(temp_file)
+    await ctx.respond('Scan started, your results will be ready soon...', ephemeral=True)
+    file = await attachment.to_file()
+    path = 'dump'
+    if not os.path.exists(path):
+        os.makedirs(path)
+    temp_file = os.path.join(path, file.filename)
+    await attachment.save(temp_file)
 
-			catalog = upload_to_datastore(result, ctx.user.id)
-			url = 'https://nook.lol/{}'.format(catalog['hash'])
-			await ctx.interaction.edit_original_response(content=f"Found {len(result.items)} items in your video.\nResults: {url}")
-		except Exception as e:
-			print(f'Failed to scan video {e}')
-			await ctx.respond('Failed to scan video. Make sure you have a valid video file.', ephemeral=True)
+    try:
+        result = scanner.scan_media(temp_file)
+    except Exception as e:
+        print(f'Failed to scan video {e}')
+        await ctx.respond('Failed to scan video. Make sure you have a valid video file.', ephemeral=True)
+
+    with contextlib.suppress(FileNotFoundError):
+        os.remove(temp_file)
+
+    catalog = upload_to_datastore(result, ctx.user.id)
+    url = 'https://nook.lol/{}'.format(catalog['hash'])
+    await ctx.interaction.edit_original_response(content=f"Found {len(result.items)} items in your video.\nResults: {url}")
+
 
 @bot.event
 async def on_ready():
@@ -79,13 +82,14 @@ async def on_ready():
     print(f"Python version: {platform.python_version()}")
     print(f"Running on: {platform.system()} {platform.release()} ({os.name})")
     print("-------------------")
-    
-@bot.slash_command(name="scan", description="Extracts your Animal Crossing catalog items and DIY recipes from a recorded video.")
-@option("attachment",discord.Attachment,description="The video to scan", required=True)
-async def scan(ctx: discord.ApplicationContext, attachment: discord.Attachment, integration: str):
-	if attachment:
-		await handle_message(ctx, attachment, integration)
-	else:
-		await ctx.respond('No attachment found.', ephemeral=True)
 
-bot.run(DISCORD_TOKEN)
+
+@bot.slash_command(name="scan", description="Extracts your Animal Crossing catalog items and DIY recipes from a recorded video.")
+@option("attachment", discord.Attachment, description="The video to scan", required=True)
+async def scan(ctx: discord.ApplicationContext, attachment: discord.Attachment, integration: str):
+    if attachment:
+        await handle_message(ctx, attachment, integration)
+    else:
+        await ctx.respond('No attachment found.', ephemeral=True)
+
+bot.run(constants.DISCORD_TOKEN)
